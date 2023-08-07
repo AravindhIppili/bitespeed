4ca2c38ddbdc8b83734d0f8ce6240fe1edccdff1
Aravindh <aravindhippili@gmail.com>
2023-08-07 18:24:04 +0530

    Pg connector setup

---
+++ src/db/connector.ts
@@ -0,0 +1,11 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432", 10),
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+});
+
+export default pool;
